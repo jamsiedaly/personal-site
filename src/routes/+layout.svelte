fc5b82518c7fc95ec31a633a5bad68dcb264918d
James Daly <jamsiedaly@gmail.com>
2023-12-01 17:40:26 +0100

    First pass of website with just a landing page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,32 @@
     import "../app.css";
 </script>
 
+<div class="navbar bg-base-100">
+    <div class="navbar-start">
+        <div class="dropdown">
+            <div tabindex="0" role="button" class="btn btn-ghost lg:hidden">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+            </div>
+            <ul class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                <li><a href="https://github.com/jamsiedaly" target="_blank">My Github</a></li>
+                <li><a href="https://twitter.com/jamsiedaly1" target="_blank">My Twitter</a></li>
+                <li><a href="https://www.linkedin.com/in/james-daly-ab1754118/" target="_blank">My LinkedIn</a></li>
+                <li><a href="https://rust.amsterdam/" target="_blank">Rust Amsterdam</a></li>
+            </ul>
+        </div>
+        <a class="btn btn-ghost text-xl" href="/">James Daly</a>
+    </div>
+    <div class="navbar-center hidden lg:flex">
+        <ul class="menu menu-horizontal px-1">
+            <li><a href="https://github.com/jamsiedaly" target="_blank">My Github</a></li>
+            <li><a href="https://twitter.com/jamsiedaly1" target="_blank">My Twitter</a></li>
+            <li><a href="https://www.linkedin.com/in/james-daly-ab1754118/" target="_blank">My LinkedIn</a></li>
+            <li><a href="https://rust.amsterdam/" target="_blank">Rust Amsterdam</a></li>
+        </ul>
+    </div>
+    <div class="navbar-end">
+        <a class="btn btn-accent" href="https://broadstack.dev/" target="_blank">Broadstack.dev</a>
+    </div>
+</div>
+
 <slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,86 @@
-<h1 class="text-3xl font-bold underline">
-    Hello world!
-</h1>
+<div class="flex flex-col w-full items-center justify-center py-4">
+    <img src="/img/meetup-fiberplane.jpeg" alt="Me at a Meetup" class="mask mask-squircle w-1/3">
 
-<style lang="postcss">
-    :global(html) {
-        background-color: theme(colors.gray.500);
-    }
-</style>
+    <div class="hero h-1/2 w-2/3 bg-base-200 my-2.5">
+        <div class="hero-content text-center">
+            <div class="max-w-md">
+                <h1 class="text-xl font-bold">About me</h1>
+                <p class="py-6">My name is James Daly. I'm a software engineer from Ireland but I live in Amsterdam
+                    these days. I'm currently working at
+                    <a href="https://www.codenomads.nl/" target="_blank" class="link px-1">Code Nomads</a>
+                    as a software developer where I develop software, provide training and lots of other stuff.
+
+                    I work primarily in the backend with languages like Rust, Kotlin and Java.
+                    I also do passable work in the frontend with Sveltekit, Angular and TypeScript.
+                </p>
+                <a class="btn btn-primary" href="https://www.codenomads.nl/contact/" target="_blank">Contact</a>
+            </div>
+        </div>
+    </div>
+
+    <div class="divider divider-secondary my-10"><h1 class="text-5xl font-bold">What I've been up to</h1></div>
+
+    <div class="hero h-1/2 w-2/3 bg-base-200">
+        <div class="hero-content flex-col lg:flex-row">
+            <img src="/img/broadstack.png" class="max-w-sm rounded-lg shadow-2xl mask mask-hexagon-2" alt="logo"/>
+            <div>
+                <h1 class="text-5xl font-bold">Broadstack</h1>
+                <p class="py-6">A real time in browser broadcast production system. I built this my free time over the
+                    course of a year to enable low budget broadcast production of sports. The project uses Sveltekit with
+                    Typescript and Rust with Actix Web and FFMPEG.</p>
+                <a class="btn btn-primary" href="https://broadstack.dev/" target="_blank">Have a look</a>
+            </div>
+        </div>
+    </div>
+
+    <div class="divider divider-accent my-10"></div>
+
+    <div class="hero h-1/2 w-2/3 bg-base-200">
+        <div class="hero-content flex-col lg:flex-row">
+            <div>
+                <h1 class="text-5xl font-bold">AutometricsJ</h1>
+                <p class="py-6">Autometrics is an observability micro-framework built for developers.
+                    It makes it easy to instrument any function with the most useful metrics:
+                    request rate, error rate, and latency.
+                    Autometrics uses instrumented function names to generate Prometheus queries so you don’t need to
+                    hand-write complicated PromQL.
+                    I have implemented a client library for Java and Kotlin along with a plugin for IntelliJ IDEA.
+                </p>
+                <a class="btn btn-primary" href="https://github.com/jamsiedaly/autometricsj" target="_blank">Library source</a>
+                <a class="btn btn-primary" href="https://github.com/jamsiedaly/autometrics-plugin" target="_blank">Plugin source</a>
+                <a class="btn btn-accent" href="https://plugins.jetbrains.com/plugin/22408-autometrics" target="_blank">Install plugin</a>
+            </div>
+            <img src="/img/autometrics-logo.png" class="max-w-sm rounded-lg shadow-2xl mask mask-hexagon bg-white" alt="logo"/>
+        </div>
+    </div>
+
+    <div class="divider divider-accent my-10"></div>
+
+    <div class="hero h-1/2 w-2/3 bg-base-200">
+        <div class="hero-content flex-col lg:flex-row">
+            <img src="/img/ferris.png" class="max-w-sm rounded-lg shadow-2xl mask mask-heart bg-white" alt="logo"/>
+            <div>
+                <h1 class="text-5xl font-bold">Rust Developers Amsterdam</h1>
+                <p class="py-6">A few years ago I started a meetup group for Rust developers in Amsterdam. We meet
+                    once every month or two to discuss Rust and related topics. We have a great community of developers and
+                    enthusiasts. I have given presentations on Rust and held workshops at the Meetup.
+                </p>
+                <a class="btn btn-primary" href="https://www.meetup.com/nl-NL/rust-amsterdam-group/" target="_blank">Join us</a>
+            </div>
+        </div>
+    </div>
+
+    <div class="divider divider-accent my-10"></div>
+
+    <div class="hero h-1/2 w-2/3 bg-base-200">
+        <div class="hero-content flex-col lg:flex-row">
+            <div>
+                <h1 class="text-5xl font-bold">Amsterdam GAC</h1>
+                <p class="py-6">Outside of tech I enjoy playing gaelic football and hurling. The national games of my
+                    home country. I serve on the Amsterdam GAC committee as the club secretary.
+                </p>
+            </div>
+            <img src="/img/gac.png" class="max-w-sm rounded-lg shadow-2xl mask mask-circle bg-white" alt="logo"/>
+        </div>
+    </div>
+</div>
